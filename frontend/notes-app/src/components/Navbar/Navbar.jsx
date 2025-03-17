50c4df416120d24dc103566e81f81d58fd3349b0
Yash Parkhe <yashc149641@gmail.com>
2025-03-18 00:01:13 +0530

    Added some changes in tailwind config and fixed that css wasn't working

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow-md'>
+    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>Notes</h2>
     </div>
   )
